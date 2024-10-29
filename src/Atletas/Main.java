76b2951198b6b48ed409227a9306b45a64509ed5
Guilherme <guilhermedelimadearaujo@gmail.com>
2024-10-28 22:31:22 -0300

    Update

--- src/Atletas/Main.java
+++ src/Atletas/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a quantidade de atletas:\n");
+        System.out.print("Digite a quantidade de atletas: ");
         int qtdAtletas = scanner.nextInt();
         scanner.nextLine();
 
@@ -16,25 +16,26 @@ public class Main {
         for (int i = 0; i < qtdAtletas; i++){
 
             System.out.println("Digite os dados do atleta numero " + (i + 1) + ":");
-
-            System.out.println("Nome: ");
-            String nome = scanner.nextLine();
+            
+            
+            System.out.print("Nome: ");
+            String nome = scanner.next();
 
 
             char sexo;
             do {
-                System.out.println("Sexo (F/M): ");
+                System.out.print("Sexo (F/M): ");
                 sexo = scanner.next().charAt(0);
                 if (sexo != 'M' && sexo != 'F'){
                     System.out.println("Valor invalido! Digite F ou M.");
                 }
-            } 
+            }
             while (sexo != 'M' && sexo != 'F');
 
 
             float altura;
             do {
-                System.out.println("Altura: ");
+                System.out.print("Altura: ");
                 altura = scanner.nextFloat();
                 if (altura <= 0){
                     System.out.println("Valor invalido! Digite um valor positivo.");
@@ -44,7 +45,7 @@ public class Main {
 
             float peso;
             do {
-                System.out.println("Peso: ");
+                System.out.print("Peso: ");
                 peso = scanner.nextFloat();
                 if (peso < 0){
                     System.out.println("Valor invalido! Digite um valor positivo.");
@@ -55,11 +56,17 @@ public class Main {
             atletas[i] = new Atleta(nome, sexo, altura, peso);
         }
 
+
         System.out.println("\n::: Relatório :::");
         System.out.printf("Peso médio dos atletas: %.2f\n", Relatorio.pesoMedio(atletas));
         System.out.println("Atleta mais alto: " + Relatorio.encAtletaMaisAlto(atletas));
+        System.out.printf("Porcentagem de homens: %.1f%%\n", Relatorio.calPorcentagemHomen(atletas));
 
-
+        float alturaMediaMulher = Relatorio.calAlturaMediaMulher(atletas);
+        if (alturaMediaMulher != -1){
+            System.out.printf("Altura média das mulheres: %.2f\n", alturaMediaMulher);
+        }
+        
         scanner.close();
     }
 
--- src/Atletas/Relatorio.java
+++ src/Atletas/Relatorio.java
@@ -10,6 +10,16 @@ public class Relatorio {
         return somaPeso / atletas.length;
     }
 
+    public static float calPorcentagemHomen(Atleta[] atletas) {
+        int qtdHomens = 0;
+        for (int i = 0; i < atletas.length; i++){
+            if (atletas[i].getSexo() == 'M'){
+                qtdHomens++;
+            }
+        } 
+        return ((float) qtdHomens / atletas.length) * 100;
+    }
+
 
     public static String encAtletaMaisAlto(Atleta[] atletas){
         Atleta maisAlto = atletas[0];
@@ -20,6 +30,24 @@ public class Relatorio {
         }
         return maisAlto.getNome();
 
+    }
+
+    public static float calAlturaMediaMulher(Atleta[] atletas){
+        int qtdMulheres = 0;
+        int somaAltura = 0;
+
+        for (int i = 0; i < atletas.length; i++){
+            if (atletas[i].getSexo() == 'F'){
+                somaAltura += atletas[i].getAltura();
+                qtdMulheres++;
+            }
+        }
+        if (qtdMulheres == 0){
+            System.out.println("Não há mulheres cadastradas.");
+            return -1;
+        }
+
+        return somaAltura / qtdMulheres;
     }
 
 }
